46421cb6a12c0216306062b43de7a95dbe6f155a
Logan Martlew <logan.martlew@gmail.com>
2021-10-13 14:58:04 +1300

    Added static file serving with default image

--- src/api/middleware/index.js
+++ src/api/middleware/index.js
@@ -5,14 +5,6 @@ const queryParams = require('./queryParams');
 module.exports = () => {
   const app = Router();
 
-  // Example middleware loading
-  // app.use(isAuth())
-
-  // app.use((req, res, next) => {
-  //   console.log('middleware');
-  //   next();
-  // });
-
   app.use(auth);
   app.use(queryParams);
 
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,3 +1,5 @@
+const { static } = require('express');
+const path = require('path');
 const cors = require('cors');
 const { json } = require('body-parser');
 const api = require('@api');
@@ -5,6 +7,8 @@ const api = require('@api');
 module.exports = async app => {
   app.use(cors());
   app.use(json());
+
+  app.use(static(path.join(__dirname, '../', 'public')));
 
   app.use(api());
 };
