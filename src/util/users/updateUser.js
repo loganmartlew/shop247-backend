e129701a6712077f50e363cd2c778f2006ab51a1
Logan Martlew <logan.martlew@gmail.com>
2021-10-13 15:03:47 +1300

    Added updateUser function

--- src/util/users/updateUser.js
+++ src/util/users/updateUser.js
@@ -1,5 +1,33 @@
 const { User } = require('@models/User');
 
-const updateUser = async () => {};
+const updateUser = async (uid, facebook, instagram, location, avatar) => {
+  try {
+    const user = await User.findOne({ uid });
+
+    if (!user) return null;
+
+    if (facebook) {
+      user.social.facebook = facebook;
+    }
+
+    if (instagram) {
+      user.social.instagram = instagram;
+    }
+
+    if (location) {
+      user.location = location;
+    }
+
+    if (avatar) {
+      user.avatar = avatar;
+    }
+
+    const newUser = await user.save();
+
+    return newUser;
+  } catch (err) {
+    return null;
+  }
+};
 
 module.exports = { updateUser };
