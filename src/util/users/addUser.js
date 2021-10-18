7ef4237e4d72e34e4ffd43890db5a9987fcadcab
Logan Martlew <logan.martlew@gmail.com>
2021-10-18 21:31:26 +1300

    Added defaults in addUser

--- src/util/users/addUser.js
+++ src/util/users/addUser.js
@@ -2,9 +2,16 @@ const { User } = require('@models/User');
 
 const addUser = async user => {
   // Add user
-  const newUser = await new User(user).save();
+  const newUser = await new User(user);
 
-  return newUser;
+  user.avatar = 'https://shop247-backend.herokuapp.com/defaultprofile.webp';
+
+  user.social = {
+    facebook: '',
+    instagram: '',
+  };
+
+  return newUser.save();
 };
 
 module.exports = { addUser };
